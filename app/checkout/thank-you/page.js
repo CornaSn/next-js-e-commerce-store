baa29659a14e62c9a66ebb597d7d2cdc6cc1a2e0
CornaSn <cornelia.schenk@hotmail.com>
2024-06-10 21:41:28 +0200

    Insert Thank You Page styling

--- app/checkout/thank-you/page.js
+++ app/checkout/thank-you/page.js
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import Logo from '../../../public/images/logo.webp';
+import styles from './ThankYou.module.scss';
+
 export const metadata = {
   title: 'Thank You',
   description:
@@ -6,13 +10,20 @@ export const metadata = {
 
 export default function ThankYou() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Thank you for your order</h1>
-      <div>
+      <p>
         Your order has been received and a confirmation has been sent to your
         e-mail.
-      </div>
-      <h2>Looking forward on seeing you at the workshop!</h2>
+      </p>
+      <p>Looking forward on seeing you at the workshop!</p>
+      <Image
+        src={Logo}
+        alt="Cornafy Yoga "
+        width={200}
+        height={200}
+        className={styles.logo}
+      />
     </div>
   );
 }
