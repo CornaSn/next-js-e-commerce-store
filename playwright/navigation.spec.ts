8e78e0617c94fc7b0a6676b1a84787f2e7709b0f
CornaSn <cornelia.schenk@hotmail.com>
2024-06-24 10:54:37 +0200

    Add end-to-end test, find heading and picture

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,5 +1,14 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('navigation test', async ({ page }) => {
   await page.goto('/');
+
+  // await expect(
+  //   page.getByRole('heading', { name: 'Welcome to Cornafy Yoga!' }),
+  // ).toBeVisible();
+
+  // test if images are visible
+  await expect(
+    page.getByRole('img', { name: /Group Yoga Image/ }).first(),
+  ).toBeVisible();
 });
